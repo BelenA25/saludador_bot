fa68315e8bde9e2d684f344176c5b2dd82d0b771
“Belen <beckiar23@gmail.com>
2023-02-19 21:50:29 -0400

    Añade saludo por nombre, genero y edad

--- src/presenter.js
+++ src/presenter.js
@@ -4,16 +4,31 @@ const nombre = document.querySelector("#nombre");
 const form = document.querySelector("#saludar");
 const sexo = document.querySelector("#box1")
 const div = document.querySelector("#saludador");
+const edad = document.querySelector("#edad")
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   if (sexo.value == "masculino")
   {
-    div.innerHTML = "Hola señor " +  nombre.value;
+    if (edad.value >30)
+    {
+      div.innerHTML = "Hola señor " +  nombre.value;
+    }
+    else
+    {
+      div.innerHTML = "Hola joven " +  nombre.value;
+    }
   }
   else
   {
-    div.innerHTML = "Hola señora " +  nombre.value;
+    if (edad.value>30)
+    {
+      div.innerHTML = "Hola señora " +  nombre.value;
+    }
+    else
+    {
+      div.innerHTML = "Hola señorita " +  nombre.value;
+    }
   }
 });
